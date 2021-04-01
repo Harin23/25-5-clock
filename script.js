a65a8cf95ebfc47c3c0aeca22a5825c245e4611d
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-04-01 17:23:12 -0600

    Auto timer switching upon timer running out implemented.

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ class App extends React.Component{
             minsLeft: 25,
             secsLeft: 0,
             paused: true,
-            started: false,
             currTimer: "Session"
         }
         this.startStopTimer=this.startStopTimer.bind(this);
         this.timer=this.timer.bind(this);
         this.switchTimer=this.switchTimer.bind(this);
-        this.reset=this.reset.bind(this);
+        this.updateEndTime=this.updateEndTime.bind(this);
+        this.resetTimer=this.resetTimer.bind(this);
         this.timerCaller;
     }
     startStopTimer(){
         if(this.state.paused==true){
-            this.reset([this.state.minsLeft, this.state.secsLeft]);
+            this.updateEndTime([this.state.minsLeft, this.state.secsLeft]);
             this.setState({paused: false});
             this.timerCaller = setInterval(this.timer, 200);
         }else{
@@ -29,18 +29,48 @@ class App extends React.Component{
     }
     timer(){
         var timeLeft=this.state.endTime-Date.now();
-        this.setState({
-            minsLeft: Math.floor(timeLeft/60000),
-            secsLeft: Math.floor((timeLeft%60000)/1000)
-        });
+        if(timeLeft>0){
+            this.setState({
+                minsLeft: Math.floor(timeLeft/60000),
+                secsLeft: Math.floor((timeLeft%60000)/1000)
+            });
+        }else{
+            clearInterval(this.timerCaller);
+            this.switchTimer()
+        }
     }
-    reset(timerEnd){
+    updateEndTime(timerEnd){
         this.setState({
             endTime: new Date().getTime()+(timerEnd[0]*60000)+(timerEnd[1]*1000)
         });
     }
+    resetTimer(){
+        this.setState(state=>({
+            endTime: 0,
+            minsLeft: state.sessionLength,
+            secsLeft: 0,
+            paused: true,
+            currTimer: "Session"
+        }))
+    }
     switchTimer(){
-
+        if(this.state.currTimer=="Session"){
+            this.setState(state=>({
+                minsLeft: state.breakLength,
+                secsLeft: 0,
+                paused: true,
+                currTimer: "Break"
+            }));
+            this.startStopTimer();
+        }else{
+            this.setState(state=>({
+                minsLeft: state.sessionLength,
+                secsLeft: 0,
+                paused: true,
+                currTimer: "Session"
+            }));
+            this.startStopTimer();
+        }
     }
     render(){
         return(
@@ -52,10 +82,10 @@ class App extends React.Component{
                     <button className="btns" id="break-decrement"><i className="fa fa-sort-down fa-3x down-icon"></i></button>
                 </div>
                 <div className="container" id="timer">
-                    <h6 id="timer-label">Session</h6>
+                    <h6 id="timer-label">{this.state.currTimer}</h6>
                     <h6 id="time-left">{this.state.minsLeft}:{("0"+this.state.secsLeft).slice(-2)}</h6>
                     <button onClick={this.startStopTimer} className="btns-timer" id="start_stop"><i className="fa fa-play fa-3x timer-icon"></i><i className="fa fa-pause fa-3x timer-icon"></i></button>
-                    <button className="btns-timer" id="reset"><i className="fa fa-undo fa-3x timer-icon"></i></button>
+                    <button onClick={this.resetTimer} className="btns-timer" id="reset"><i className="fa fa-undo fa-3x timer-icon"></i></button>
                 </div>
                 <div className="container" id="session-time">
                     <button className="btns" id="session-increment"><i className="fa fa-sort-up fa-3x up-icon"></i></button>
